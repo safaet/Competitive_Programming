fade6b89c750c2bba71cc66736703977a9568be7
safaet <safaetjaman@gmail.com>
2024-03-25 02:37:46 +0600

    Add Book of DS and CP

--- leetcode/Top_Interview_150/find-the-duplicate-number.py
+++ leetcode/Top_Interview_150/find-the-duplicate-number.py
@@ -1,16 +1,21 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
+    def findDuplicate(self, nums):
         li = [0] * len(nums)
         ans= 0
 
         for num in nums:
-            li[num] ++
+            li[num] = li[num] + 1
 
-        for num in li:
+        for num in nums:
             if li[num] > 1:
                 ans = num
                 break
 
         return ans
 
+
+num = [1, 2, 3, 3, 3]
+sol = Solution()
+
+print(sol.findDuplicate(num))
 
