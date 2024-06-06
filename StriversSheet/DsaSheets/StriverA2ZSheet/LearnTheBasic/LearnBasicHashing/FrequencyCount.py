15e2f787d6381bb823da0d839515b4629f502bb3
safaet <safaetjaman@gmail.com>
2024-06-06 23:35:39 +0600

    It also worked

--- StriversSheet/DsaSheets/StriverA2ZSheet/LearnTheBasic/LearnBasicHashing/FrequencyCount.py
+++ StriversSheet/DsaSheets/StriverA2ZSheet/LearnTheBasic/LearnBasicHashing/FrequencyCount.py
@@ -1,17 +1,23 @@
 class Solution:
 
     def frequencyCount(self, arr, N, P):
-        P += 1
+        count = 0
+        dic = {}
         for i in range(N):
-            idx = arr[i] % P
-            if idx <= N:
-                arr[idx-1] += P
+            if i+1 not in dic:
+                dic[i+1] = 0
+            if arr[i] in dic:
+                dic[arr[i]] +=1
+            elif arr[i] <= N:
+                dic[arr[i]] = 1
 
-        for i in range(N):
-            arr[i] = arr[i]//P
+        li = sorted(dic.items())
+        lis=[]
+
+        for i in range(len(li)):
+            arr[i] = li[i][1]
+
         
-
 arr = [2, 3, 2, 3, 5]
 Solution().frequencyCount(arr, 5, 5) # 1
-
 print(arr)
